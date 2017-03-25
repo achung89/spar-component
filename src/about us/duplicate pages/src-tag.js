8a0321dcda51961ed22509753f3e1ddddc41ff06
Andrew Chung <chung.andrew7@gmail.com>
2017-03-25 00:07:29 -0700

    getting duplicat page tests to run

--- src/about us/duplicate pages/src-tag.js
+++ src/about us/duplicate pages/src-tag.js
@@ -3,8 +3,7 @@ var eles = document.querySelector('.test-class');
 if (eles) {
   eles.forEach ( ele => {
     ele.innerHTML='querySelector selects across shadowDOM!';
+    console.log('The specs have changed, update application')
   })
 }
-console.log("i'm the duplicate tag")
-
-document.querySelector('.home-dup.class-div-2').shadowRoot.querySelector('.test-class-2').innerHTML='class selected';
+document.querySelector('.home.class-div-2').shadowRoot.querySelector('.test-class-2').innerHTML='class selected';

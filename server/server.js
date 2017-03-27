9c80ddb5e23d81d614206a345cd10435ec45a934
Andrew Chung <chung.andrew7@gmail.com>
2017-03-27 08:35:28 -0700

    moved test directory

--- server/server.js
+++ server/server.js
@@ -5,18 +5,18 @@ var app = express();
 var appStatAssets = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../test-directory/')));
 
 router1.get('/', function ( req, res ) {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../test-directory/index.html'));
 });
 router2.get('/', function ( req,res ) {
-  res.sendFile(path.join(__dirname, '../index2/index.html'));
+  res.sendFile(path.join(__dirname, '../test-directory/index2/index.html'));
 })
 
 app.use(router1);
 app.use('/two', router2)
-app.use('/three', express.static(path.join(__dirname, '../server_assets')));
+app.use('/three', express.static(path.join(__dirname, '../test-directory/server_assets')));
 
 app.listen(8080, function(err){
   if(err) {
